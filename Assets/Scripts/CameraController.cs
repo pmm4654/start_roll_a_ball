783d52f0964dbec830510321e3b2c63e2913b665
pmm4654 <pmm4654@gmail.com>
2018-04-29 17:19:54 -0400

    Roll-a-ball with accelerometer stuff

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // transform position of camera - transform position of player
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame AND guaranteed to run after all items have been processed in update
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movements = new Vector3(15, 30, 45) * Time.deltaTime;
+        transform.Rotate(movements);
+	}
+}
